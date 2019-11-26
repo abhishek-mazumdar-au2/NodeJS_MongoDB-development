69fd8006959496b4f7fc054d37e7567d9e5ace48
Abhishek Mazumdar <abhishekmazumdar118@gmail.com>
2019-11-26 12:16:40 +0530

    MongoDB Query documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -4,9 +4,9 @@ const { MongoClient, ObjectID } = require('mongodb');
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
-const id = new ObjectID()
-console.log(id);
-console.log(id.getTimestamp());
+// const id = new ObjectID()
+// console.log(id);
+// console.log(id.getTimestamp());
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if(error){
@@ -40,22 +40,40 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 //     console.log(result.ops)
 //    })
 
-db.collection('tasks').insertMany([
-    {
-        description: "make the bed",
-        status: true
-    },
-    {
-        description: "drop the garbage",
-        status: false
-    },{
-        description: "make tea",
-        status: false
-    }
-], (error, result) => {
-    if(error){
-        return console.log(error);
-    }
-    console.log(result.ops)
-})
+// db.collection('tasks').insertMany([
+//     {
+//         description: "make the bed",
+//         status: true
+//     },
+//     {
+//         description: "drop the garbage",
+//         status: false
+//     },{
+//         description: "make tea",
+//         status: false
+//     }
+// ], (error, result) => {
+//     if(error){
+//         return console.log(error);
+//     }
+//     console.log(result.ops)
+// })
+// db.collection('tasks').
+//     find({ status: false }).
+//        toArray((error, tasks)=> {
+//     if(error){
+//         return console.log(error);
+//     }console.log(tasks)
+// })
+db.collection('tasks').
+    findOne({ _id: ObjectID("5ddc30df0d30fd544d259ca7")}, (error, task) => {
+        if(error) return console.log(error);
+        console.log(task);
+    })
+         
+    db.collection('users').
+       find({ age: {$lt: 24} }).
+           toArray((error, array) => {
+               console.log(array);
+           })
 })
